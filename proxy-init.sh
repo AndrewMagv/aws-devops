5820eda58cf055391c5c81963da941f74de3d0ca
Jeffrey Jen <yihungjen@gmail.com>
2015-11-11 02:30:46 +0000

    NEW: stunnel proxy bootstrap script

---
+++ proxy-init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ex
+
+PREFIX=s3://devops.magv.com/launch-config/tlsproxy
+
+# obtain TLS connection options
+aws s3 cp --region ${NODE_REGION} ${PREFIX}/ca.pem /cert/ca.pem
+aws s3 cp --region ${NODE_REGION} ${PREFIX}/server-key.pem /cert/server-key.pem
+aws s3 cp --region ${NODE_REGION} ${PREFIX}/server-cert.pem /cert/server-cert.pem
+
+# download trusted cert and hash
+mkdir -p /trusted
+rm -f /trusted/*
+aws s3 cp --region ${NODE_REGION} ${PREFIX}/cert.pem /trusted/cert.pem
+HASHVALUE=$(openssl x509 -noout -hash -in /trusted/cert.pem)
+ln -s /trusted/cert.pem /trusted/${HASHVALUE}.0
+
+URL="https://raw.githubusercontent.com/AndrewMagv/aws-devops/${REF}/tlsproxy"
+while [ $# -gt 0 ]; do
+    case ${1} in
+        redis)
+            curl -sSL ${URL}/redis.conf >>/proxy.conf
+            ;;
+        etcd)
+            curl -sSL ${URL}/etcd.conf >>/proxy.conf
+            ;;
+        *)
+            echo "Unexpected service type: ${1}"
+            ;;
+    esac
+    shift 1 # moving along
+done
