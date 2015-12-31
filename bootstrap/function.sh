87304e5e7df44142887d1099ca0a24c30e6a75fe
Jeffrey Jen <yihungjen@gmail.com>
2015-12-31 06:48:03 +0000

    NEW: install admin toolkit - 1

--- bootstrap/function.sh
+++ bootstrap/function.sh
@@ -30,6 +30,7 @@ apt-get update && apt-get install -y docker-engine
 get-toolkit() {
 #install docker tool kit
 curl -sSL https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 }
 
 config-docker-engine() {
